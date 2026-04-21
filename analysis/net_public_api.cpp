19ec3a1cf17238a9e854c0be03b1f28e5e1b0984
YYZ-YingSe <3223942615hy@gmail.com>
2026-04-22 06:22:25 +0800

    refactor: remove net surface

--- analysis/net_public_api.cpp
+++
@@ -1,5 +0,0 @@
-#include "wh/net/concepts.hpp"
-#include "wh/net/http_client_interface.hpp"
-#include "wh/net/types.hpp"
-
-auto main() -> int { return 0; }
--- include/wh/net/concepts.hpp
+++
@@ -1,8 +0,0 @@
-// Aggregates all compile-time net adapter concepts.
-#pragma once
-
-#include "wh/net/concepts/connection_pool_concept.hpp"
-#include "wh/net/concepts/dns_cache_concept.hpp"
-#include "wh/net/concepts/http_client_concept.hpp"
-#include "wh/net/concepts/memory_pool_concept.hpp"
-#include "wh/net/concepts/sse_parser_concept.hpp"
